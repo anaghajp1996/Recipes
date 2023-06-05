857f58877801eeb4d4eb1377827a1485a268febc
Anagha <anaghajayaprakash1996@gmail.com>
2023-06-05 11:38:29 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Anagha K J on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
